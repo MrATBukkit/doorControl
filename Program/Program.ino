c77be45e49ade5ec9558b9a1f42ad422d68e8b4b
Hermann <hewalzl999@gmail.com>
2019-01-26 21:40:39 +0100

    HTML

--- Program/Program.ino
+++ Program/Program.ino
@@ -3,6 +3,9 @@
 
 const char* ssid = "Hermann P20";//type your ssid
 const char* password = "abcd&Internet@4030";//type your password
+
+/*const char* ssid = "e166";//type your ssid
+const char* password = "Internet";//type your password*/
  
 int ledPin = 0; // GPIO2 of ESP8266
 int doorPin = 2;
@@ -14,11 +17,16 @@ void setup() {
   pinMode(ledPin, INPUT);
   pinMode(doorPin, OUTPUT);
   digitalWrite(doorPin, LOW);
-   
+
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  IPAddress ip(192,168,0,230);   
+  /*IPAddress ip(192,168,0,230);   
   IPAddress gateway(192,168,0,1);   
+  IPAddress subnet(255,255,255,0);   
+  //WiFi.config(ip, gateway, subnet);*/
+    IPAddress ip(192,168,2,200);   
+  IPAddress gateway(192,168,2,254);   
   IPAddress subnet(255,255,255,0);   
   //WiFi.config(ip, gateway, subnet);
   
@@ -49,23 +57,20 @@ void loop() {
 
   client.flush();
 
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>"); 
-  client.println("<html>");
   if (request.indexOf("/status") != -1) {
-    client.println("Status: ");
     client.println(digitalRead(ledPin));
   } 
-  if (request.indexOf("/toggle") != -1) {
+  else if (request.indexOf("/toggle") != -1) {
     digitalWrite(doorPin, HIGH);
     delay(800);
     digitalWrite(doorPin, LOW);
     client.println("toggle");
   } 
+  else if (request.indexOf("/") != -1) {
+    String htmlCode = "<!DOCTYPE html><html lang='de'><head> <meta charset='UTF-8'> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <meta http-equiv='X-UA-Compatible' content='ie=edge'> <link href='https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' rel='stylesheet' id='bootstrap-css'> <title>Document</title> <style>.container{height: 100vh; padding: 0px; margin: 0px;}.btn-circle{width: 30px; height: 30px; text-align: center; padding: 6px 0; font-size: 12px; line-height: 1.428571429; border-radius: 15px; margin-left: 25vw; margin-top: calc((100vh - 50vw) / 2)}.btn-circle.btn-xl{width: 50vw; height: 50vw; padding: 10px 16px; font-size: 24px; line-height: 1.33; border-radius: 100px; margin-right: 50vw;}button{margin: 30px auto; text-align: center; display: flex; align-items: center; margin-bottom: 0px !important; margin-right: 0px !important;}</style></head><body> <div class='container'> <button class='btn btn-danger btn-circle btn-xl status' id='toggle'> Schalten </button> </div></body><script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js'></script><script>function setStatus(){axios.get('/status').then(function(response){document.querySelector('.status').innerHTML=response.data? 'Öffnen': 'Schließen'; if (response.data){document.querySelector('#toggle').classList.add('btn-danger'); document.querySelector('#toggle').classList.remove('btn-success');}else{document.querySelector('#toggle').classList.add('btn-success'); document.querySelector('#toggle').classList.remove('btn-danger');}}).catch(()=>{document.querySelector('.status').innerHTML='Fehler';});}setStatus(); setInterval(setStatus, 3000); document.querySelector('#toggle').addEventListener('click', function(){axios.get('/toggle').then(function(response){console.log('toggle');});});</script></html>";
+    client.println(htmlCode);
+  }
   // Return the response 
-  client.println("</html>");
   delay(1);
  
-}
+} 
